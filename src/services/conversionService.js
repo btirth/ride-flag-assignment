ca458635f77ab9367a06ce286af5bb6fc348a36d
Tirth Bharatiya <emailtirth@gmail.com>
2025-02-23 14:22:12 -0400

    Add: field in validation error and unit tests

--- src/services/conversionService.js
+++ src/services/conversionService.js
@@ -3,6 +3,7 @@ const loadExchangeRates = require("../utils/exchangeRateLoader");
 const logger = require("../utils/logger");
 
 let exchangeRates = [];
+let supportedCurrencies = [];
 
 /**
  * Loads the exchange rates from the CSV file
@@ -10,6 +11,9 @@ let exchangeRates = [];
 const initPromise = loadExchangeRates()
   .then((rates) => {
     exchangeRates = rates;
+    supportedCurrencies = [
+      ...new Set(exchangeRates.map((rate) => rate.currency)),
+    ];
   })
   .catch((error) => {
     logger.error(`Failed to load exchange rates: ${error.message}`);
@@ -22,21 +26,49 @@ const initPromise = loadExchangeRates()
  * @param {*} amount_in_cad - Amount in CAD to convert based on given currency and date
  * @returns {Array<Object>} - Validation result with errors or validated amount in CAD
  */
-exports.validateRequest = (date, currency, amount_in_cad) => {
+exports.validateRequest = async (date, currency, amount_in_cad) => {
+  await initPromise;
   const validationErrors = [];
 
-  if (!date) validationErrors.push("date is required");
-  if (!currency) validationErrors.push("currency is required");
-  if (!amount_in_cad) validationErrors.push("amount_in_cad is required");
+  if (!date) {
+    validationErrors.push({
+      field: "date",
+      message: "Date is required field.",
+    });
+  } else if (!isValid(parseISO(date))) {
+    validationErrors.push({
+      field: "date",
+      message:
+        "Invalid date, Please provide valid date in 'YYYY-MM-DD' format.",
+    });
+  }
 
-  if (date && !isValid(parseISO(date)))
-    validationErrors.push(
-      "Invalid date, Please provide valid date in 'YYYY-MM-DD' format"
-    );
+  if (!currency) {
+    validationErrors.push({
+      field: "currency",
+      message: "currency is required field.",
+    });
+  } else if (!supportedCurrencies.includes(currency)) {
+    validationErrors.push({
+      field: "currency",
+      message: `Currency: ${currency}, is not supported at this moment.`,
+    });
+  }
+
+  if (!amount_in_cad) {
+    validationErrors.push({
+      field: "amount_in_cad",
+      message: "amount_in_cad is required field.",
+    });
+  }
 
   const amountInCAD = parseFloat(amount_in_cad);
-  if (amount_in_cad && isNaN(amountInCAD))
-    validationErrors.push("Invalid amount");
+  if (amount_in_cad && isNaN(amountInCAD)) {
+    validationErrors.push({
+      field: "amount_in_cad",
+      message: "Invalid amount.",
+    });
+  }
 
   if (validationErrors.length > 0)
     return { valid: false, errors: validationErrors };
@@ -57,19 +89,17 @@ const findExchangeRate = (date, currency) => {
 };
 
 exports.convertCurrency = async (date, currency, amountInCAD) => {
-  await initPromise;
-
   const exchangeRateData = findExchangeRate(date, currency);
   if (!exchangeRateData) {
     return {
       statusCode: 404,
-      error: "Exchange rate not found for the given date and currency",
+      error: "Exchange rate not found for the given date and currency.",
     };
   }
 
   const exchangeRate = parseFloat(exchangeRateData.value);
   if (isNaN(exchangeRate)) {
-    return { statusCode: 500, error: "Invalid exchange rate" };
+    return { statusCode: 500, error: "Invalid exchange rate." };
   }
 
   const convertedAmount = parseFloat((amountInCAD / exchangeRate).toFixed(4));
